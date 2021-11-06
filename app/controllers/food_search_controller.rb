9a71488b5c212a98cf0959fe3b36e31fdce29f62
sami-p <sami.peterson14@gmail.com>
2021-11-06 16:51:52 -0600

    Creat index action

--- app/controllers/food_search_controller.rb
+++ app/controllers/food_search_controller.rb
@@ -1,6 +1,11 @@
 class FoodSearchController < ApplicationController
 
   def index
-    
+    if params[:search].blank? || BackendFacade.food_search(params[:search]).empty?
+      flash[:notice] = 'Invalid search. Please try again.'
+      redirect_to food_search_path
+    elsif params[:search]
+      BackendFacade.food_search(params[:search])
+    end
   end
 end
