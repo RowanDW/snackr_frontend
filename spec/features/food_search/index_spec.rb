778870989abd78c3159f4abb7803b35d5669daee
sami-p <sami.peterson14@gmail.com>
2021-11-06 18:02:58 -0600

    Add sad path testing

--- spec/features/food_search/index_spec.rb
+++ spec/features/food_search/index_spec.rb
@@ -1,18 +1,35 @@
 require 'rails_helper'
 
 RSpec.describe 'Food Search Page' do
-  before :each do
-    visit food_search_path
-
-    fill_in :food_search, with: "Reese's Puffs"
-
-    click_on 'Search'
+  it 'returns a list of foods based on search params' do
+    stub_request(:get, food_search_path("Reese's Puffs")).
+      to_return(:body => {
+                          "data": [
+                            {
+                              "type": "food",
+                              "attributes": {
+                                "name": "Reese's Puffs",
+                                "id": "rpuffs12",
+                              }
+                            },
+                            {
+                              "type": "food",
+                              "attributes": {
+                                "name": "Reese's Pieces",
+                                "id": "rpieces13",
+                              }
+                            }
+                            ]
+                          })
+    save_and_open_page
+    expect(page).to have_content("Reese's Pieces") #
   end
-  it 'returns a list of foods based on search params', :vcr do
-    expect(page).to have_content('Results') #
+
+  it 'will return an error message if food sear' do
+    expect(page).to have_content("Invalid search. Please try again.")
   end
 
-  it 'displays a list of users favorite foods' do
+  xit 'displays a list of users favorite foods' do
   end
 end
 # Use Webmock instead of VCR because we do not have access to API yet
