4298e65477770e70a5481f5530f1397bcfa3391e
Michael Mattson <michael.p.mattson@gmail.com>
2021-11-07 12:10:13 -0700

    Add foods instance var using cookie, add delete button, fixture data for meal

--- app/controllers/meal_builder_controller.rb
+++ app/controllers/meal_builder_controller.rb
@@ -1,5 +1,6 @@
 class MealBuilderController < ApplicationController
   def index
-
+    meal   = JSON.parse(cookies[:meal], symbolize_names: true)[:data]
+    @foods = meal[:attributes][:foods]
   end
 end
