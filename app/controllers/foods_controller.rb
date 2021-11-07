5f001c6101f37cfb94de3119250a76ea8f538199
sami-p <sami.peterson14@gmail.com>
2021-11-07 16:43:31 -0700

    Finish conditional for add food to existing meal

--- app/controllers/foods_controller.rb
+++ app/controllers/foods_controller.rb
@@ -1,12 +1,15 @@
 class FoodsController < ApplicationController
 
   def create
+    food_params = {food_id: params[:food_id], name: params[:food_name]}
     if cookies[:meal].nil?
-      new_meal = Meal.new({}) # build a new meal
-      require "pry"; binding.pry
+      meal = Meal.new({})
     else
-      # add food to an existing meal
+      meal_data  = JSON.parse(cookies[:meal], symbolize_names: true)[:data]
+      meal       = Meal.new(meal_data, meal_data[:attributes][:foods])
     end
+    meal.add_food_entry(food_params)
+    reset_meal(meal)
     redirect_to(meal_builder_path)
   end
 
@@ -22,7 +25,7 @@ class FoodsController < ApplicationController
   private
 
   def reset_meal(meal)
-    cookies[:meal].clear
+    cookies[:meal]&.clear
     cookies[:meal] = MealSerializer.new(meal).serializable_hash.to_json
   end
 end
