26fb5554095019fd3b0b07a4c3f2cda448fcc550
sami-p <sami.peterson14@gmail.com>
2021-11-07 11:58:14 -0700

    Temporarily remove conditional for sad path

--- app/controllers/food_search_controller.rb
+++ app/controllers/food_search_controller.rb
@@ -1,11 +1,8 @@
 class FoodSearchController < ApplicationController
 
   def index
-    if params[:search].blank? || BackendFacade.food_search(params[:search]).empty?
-      flash[:notice] = 'Invalid search. Please try again.'
-      redirect_to food_search_path
-    elsif params[:search]
-      BackendFacade.food_search(params[:search])
+    if params[:search]
+      @foods = BackendFacade.food_search(params[:search])
     end
   end
 end
