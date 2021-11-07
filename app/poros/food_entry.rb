e9e6309b4162d0ad58bce8a77cad03a84a5ddac3
Michael Mattson <michael.p.mattson@gmail.com>
2021-11-07 14:51:08 -0700

    Add food_id instance var

--- app/poros/food_entry.rb
+++ app/poros/food_entry.rb
@@ -1,8 +1,10 @@
 class FoodEntry
-  attr_reader :id, :name, :average_rank
+  attr_reader :id, :name, :food_id, :average_rank
+  
   def initialize(food_attrs)
-    @id = food_attrs[:id]
-    @name = food_attrs[:name]
+    @id           = food_attrs[:id]
+    @name         = food_attrs[:name]
+    @food_id      = food_attrs[:food_id] #edamam
     @average_rank = food_attrs[:average_rank]
   end
 end
