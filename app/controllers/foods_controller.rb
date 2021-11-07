1d80c434002b028f2911969f2614fb3b769e0bc5
Michael Mattson <michael.p.mattson@gmail.com>
2021-11-07 14:56:23 -0700

    Add destroy method, helper

---
+++ app/controllers/foods_controller.rb
@@ -0,0 +1,17 @@
+class FoodsController < ApplicationController
+  def destroy
+    meal_data  = JSON.parse(cookies[:meal], symbolize_names: true)[:data]
+    meal       = Meal.new(meal_data, meal_data[:attributes][:foods])
+
+    meal.remove_food_by_id(params[:id].to_i)
+    reset_meal(meal)
+    redirect_to(meal_builder_path)
+  end
+
+  private
+
+  def reset_meal(meal)
+    cookies[:meal].clear
+    cookies[:meal] = MealSerializer.new(meal).serializable_hash.to_json
+  end
+end
