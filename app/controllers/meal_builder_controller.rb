34b89c7e6bb4176d9d11007ab620f3d0f9a7aac0
Michael Mattson <michael.p.mattson@gmail.com>
2021-11-07 14:53:37 -0700

    Build index

--- app/controllers/meal_builder_controller.rb
+++ app/controllers/meal_builder_controller.rb
@@ -1,5 +1,7 @@
 class MealBuilderController < ApplicationController
   def index
+    return if cookies[:meal].nil?
+
     meal   = JSON.parse(cookies[:meal], symbolize_names: true)[:data]
     @foods = meal[:attributes][:foods]
   end
