414ac26e0dc96e1c6de31685db997a0b566cc796
Kelsey Thompson <kelsthompson2@gmail.com>
2021-11-09 16:27:14 -0700

    Add graph variable to controller

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -3,5 +3,6 @@ class DashboardController < ApplicationController
   def index
     @today = DateTime.current
     @meals = BackendFacade.get_meals(current_user_id)
+    @graphs = BackendFacade.get_graphs(current_user_id)
   end
 end
