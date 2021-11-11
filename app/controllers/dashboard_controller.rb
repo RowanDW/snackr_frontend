35839c06fbbb455c2a887b9f784f1ee75c2661e8
Rowan DeLong <rowanwinzer@gmail.com>
2021-11-11 10:47:16 -0700

    Fix timezone issue

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,6 +1,6 @@
 class DashboardController < ApplicationController
   def index
-    @today = DateTime.current.in_time_zone.to_datetime
+    @today = DateTime.current.in_time_zone(Time.now.getlocal.zone)
     @meals = BackendFacade.get_meals(current_user_id)
     @graphs = BackendFacade.get_graphs(current_user_id)
   end
